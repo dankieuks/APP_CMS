04abf6d8dccdbf635fb135b7443356155bf4fab1
David <kieudinhdan02@gmail.com>
2024-11-21 02:23:25 +0700

    base iu app

--- app/(tabs)/shop.tsx
+++ app/(tabs)/shop.tsx
@@ -5,7 +5,7 @@ import ParallaxScrollView from "@/components/ParallaxScrollView";
 import { ThemedText } from "@/components/ThemedText";
 import { ThemedView } from "@/components/ThemedView";
 
-export default function HomeScreen() {
+export default function Shop() {
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
@@ -17,13 +17,12 @@ export default function HomeScreen() {
       }
     >
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Huy Đẹp Trai !hsdhjfh</ThemedText>
+        <ThemedText type="title">GUt</ThemedText>
         <HelloWave />
       </ThemedView>
     </ParallaxScrollView>
   );
 }
-
 const styles = StyleSheet.create({
   titleContainer: {
     flexDirection: "row",
